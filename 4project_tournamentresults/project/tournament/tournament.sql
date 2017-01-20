f44f0a2ce5340fc0bb0ffe2820232e246da18df6
jovanikimble <jovani073@gmail.com>
2017-01-20 09:51:55 -0800

    Completed project 4

--- 4project_tournamentresults/project/tournament/tournament.sql
+++ 4project_tournamentresults/project/tournament/tournament.sql
@@ -6,4 +6,18 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+DROP TABLE IF EXISTS players;
+CREATE TABLE players (
+  id serial,
+  name text,
+  matches integer,
+  wins integer
+);
 
+DROP TABLE IF EXISTS players;
+CREATE TABLE matches (
+  id serial,
+  pid1 integer,
+  pid2 integer,
+  winner integer
+);
